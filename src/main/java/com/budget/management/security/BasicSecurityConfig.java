2c6758a352cbf27080cebd0f52b1a75190cf721a
luizavramos <luizavramos30@gmail.com>
2022-08-22 12:40:45 -0300

    fix

--- src/main/java/com/budget/management/security/BasicSecurityConfig.java
+++ src/main/java/com/budget/management/security/BasicSecurityConfig.java
@@ -24,7 +24,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService);
         auth.inMemoryAuthentication()
                 .withUser("root")
-                .password(passwordEncoder().encode("password"))
+                .password(passwordEncoder().encode("root"))
                 .authorities("ROLE_USER");
     }
     @Bean
