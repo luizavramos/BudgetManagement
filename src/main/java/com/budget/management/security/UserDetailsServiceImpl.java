1db0219eb8a02a436baf97447ff9e704f6db91b4
luizavramos <luizavramos30@gmail.com>
2022-08-23 14:36:30 -0300

    Enabling security

--- src/main/java/com/budget/management/security/UserDetailsServiceImpl.java
+++ src/main/java/com/budget/management/security/UserDetailsServiceImpl.java
@@ -3,22 +3,25 @@ package com.budget.management.security;
 import com.budget.management.model.UserData;
 import com.budget.management.repository.UserDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-//@Service
-public class UserDetailsServiceImpl {//implements UserDetailsService {
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
 
-//    @Autowired
-//    private UserDataRepository repository;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String userName)
-//            throws UsernameNotFoundException {
-//        Optional<UserData> user = repository.findByUser(userName);
-//
-//        user.orElseThrow(() -> new UsernameNotFoundException(userName + " Not found. "));
-//
-//        return user.map(UserDetailsImpl::new).get();
-//    }
+    @Autowired
+    private UserDataRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName)
+            throws UsernameNotFoundException {
+        Optional<UserData> user = repository.findByUser(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException(userName + " Not found. "));
+
+        return user.map(UserDetailsImpl::new).get();
+    }
 }
