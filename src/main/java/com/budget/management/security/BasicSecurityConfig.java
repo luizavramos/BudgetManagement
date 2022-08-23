2b92838866b26b66daec00fdc288e5a3e12af7fd
luizavramos <luizavramos30@gmail.com>
2022-08-23 13:47:32 -0300

    Adding .properties

--- src/main/java/com/budget/management/security/BasicSecurityConfig.java
+++ src/main/java/com/budget/management/security/BasicSecurityConfig.java
@@ -2,18 +2,18 @@ package com.budget.management.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -29,6 +29,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
                 .password(passwordEncoder().encode("root"))
                 .authorities("ROLE_USER");
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
